76ca50c5f9dad4acdb896340567b44fbd6ceca1d
tanishabhatnagar <tanisha.bhatnagar12@gmail.com>
2025-06-19 19:08:13 +0530

    adding deployed backend url

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 
 import { io } from "socket.io-client";
 
-const socket = io("https://polling-website.onrender.com");
+const socket = io("https://intervuepoll.onrender.com");
 
 export default socket;
